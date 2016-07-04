3f8de77b401a8e50b497b4106d4c7c5752b91178
Uli Kusterer <ulivc@zathras.de>
2016-07-04 14:51:04 +0200

    Initial Commit

---
+++ bugmatic/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bugmatic
+//
+//  Created by Uli Kusterer on 04/07/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
