b7ae79df4c86edef085f40eb43b0e08dae5c4db1
Uli Kusterer <ulivc@zathras.de>
2016-07-16 15:42:07 +0200

    Code for parsing Github's "Link" paging header.

--- bugmatic/url_request.cpp
+++ bugmatic/url_request.cpp
@@ -99,3 +99,109 @@ size_t url_reply::append_header( void* ptr, size_t size, size_t nmemb, void* str
     }
     return data_size;
 }
+
+
+std::map<std::string,std::string>	url_reply::header_map() const
+{
+	std::map<std::string,std::string>	outMap;
+	
+	for( const std::string& currHeader : mReceivedHeaders )
+	{
+		std::pair<std::string,std::string>	splitHeader = header_name_and_value( currHeader );
+		outMap.insert( splitHeader );
+	}
+	
+	return outMap;
+}
+
+
+std::string	url_reply::link_header_rel( std::string relValue ) const
+{
+	std::map<std::string,std::string>	headers = header_map();
+	std::vector<std::string>	links = url_reply::header_list_items( headers["Link"] );
+	for( const std::string& currLink : links )
+	{
+		std::string							url;
+		std::map<std::string,std::string>	attributes = url_reply::list_item_attributes( currLink, url );
+		if( attributes["rel"].compare(relValue) == 0 )
+			return url;
+	}
+	
+	return std::string();
+}
+
+
+std::string	url_reply::header_name( std::string inHeaderLine )
+{
+	off_t pos = inHeaderLine.find(':');
+	if( pos != std::string::npos )
+		return inHeaderLine.substr(0,pos);
+	else
+		return std::string();
+}
+
+
+std::pair<std::string,std::string>	url_reply::header_name_and_value( std::string inHeaderLine )
+{
+	std::pair<std::string,std::string>	theResult;
+	off_t pos = inHeaderLine.find(':');
+	if( pos != std::string::npos )
+	{
+		theResult.first = inHeaderLine.substr(0,pos);
+		off_t	endPos = inHeaderLine.find_last_not_of( '\n' );
+		off_t	endPos2 = inHeaderLine.find_last_not_of( '\r' );
+		if( endPos > endPos2 )
+			endPos = endPos2;
+		theResult.second = inHeaderLine.substr(pos +1, endPos -(pos +1));
+	}
+	else
+		theResult.second = inHeaderLine;
+	return theResult;
+}
+
+
+std::vector<std::string>	url_reply::header_list_items( std::string inHeaderLine )
+{
+	std::vector<std::string>	theResult;
+	off_t						searchPos = 0;
+	while( true )
+	{
+		off_t pos = inHeaderLine.find(',', searchPos);
+		theResult.push_back( inHeaderLine.substr(searchPos,pos -searchPos) );
+		if( pos == std::string::npos )
+			break;
+		searchPos = pos +1;
+	}
+	return theResult;
+}
+
+
+std::map<std::string,std::string>	url_reply::list_item_attributes( std::string inHeaderLine, std::string& outFirstItem )
+{
+	std::map<std::string,std::string>	theResult;
+	off_t								searchPos = inHeaderLine.find_first_not_of( ' ', 0 );
+	off_t								pos = inHeaderLine.find(';', searchPos);
+	outFirstItem = inHeaderLine.substr(searchPos,pos -searchPos);
+	if( outFirstItem.length() >= 2 && outFirstItem[0] == '<' && outFirstItem[outFirstItem.length() -1] == '>' )
+		outFirstItem = outFirstItem.substr( 1, outFirstItem.length() -2 );
+	if( pos != std::string::npos )
+	{
+		searchPos = inHeaderLine.find_first_not_of( ' ', pos +1 );
+		while( true )
+		{
+			pos = inHeaderLine.find('=', searchPos);
+			std::string		key = inHeaderLine.substr(searchPos,pos -searchPos);
+			searchPos = inHeaderLine.find_first_not_of( ' ', pos +1 );
+			off_t			nextAttrPos = inHeaderLine.find(';', searchPos);
+			std::string		val = inHeaderLine.substr(searchPos,nextAttrPos -searchPos);
+			if( val.length() >= 2 && val[0] == '"' && val[val.length() -1] == '"' )
+				val = val.substr( 1, val.length() -2 );
+			theResult[key] = val;
+			if( pos == std::string::npos )
+				break;
+			searchPos = pos +1;
+		}
+	}
+	return theResult;
+}
+
