3fcd7f9b6cf60cd516b9d1a6d22ec3ce3e346f8d
Uli Kusterer <ulivc@zathras.de>
2016-07-16 14:06:44 +0200

    Now also retrieves comments and generates a directory structure of JSON fragments for individual issues and comments for easier merging & conflict resolution in Git.

--- bugmatic/main.cpp
+++ bugmatic/main.cpp
@@ -13,10 +13,28 @@
 #include "url_request.hpp"
 #include "json11.hpp"
 #include <fstream>
+#include <sstream>
+#include <sys/stat.h>
 
 
 using Json = json11::Json;
 using namespace std;
+
+
+string	file_contents( ifstream& stream )
+{
+	string	replyData;
+	while( true )
+	{
+		int	currCh = stream.get();
+		if( currCh == EOF )
+			break;
+		replyData.append(1, currCh);
+		if( stream.eof() )
+			break;
+	}
+	return replyData;
+}
 
 
 int main( int argc, const char * argv[] )
@@ -31,15 +49,7 @@ int main( int argc, const char * argv[] )
 		if( downloadedjson.is_open() )
 		{
 			cout << "Using cached data from 'downloaded.json'." << endl;
-			while( true )
-			{
-				int	currCh = downloadedjson.get();
-				if( currCh == EOF )
-					break;
-				replyData.append(1, currCh);
-				if( downloadedjson.eof() )
-					break;
-			}
+			replyData = file_contents( downloadedjson );
 		}
 		else
 		{
@@ -70,7 +80,11 @@ int main( int argc, const char * argv[] )
 		}
 
 		//logfile << "<<" << replyData << ">>" << endl;
-
+		
+		mkdir("issues",0777);
+		mkdir("milestones",0777);
+		mkdir("users",0777);
+		
 		string	errMsg;
 		Json	replyJson = Json::parse( replyData, errMsg );
 		if( errMsg.length() == 0 )
@@ -84,7 +98,75 @@ int main( int argc, const char * argv[] )
 					logfile << " [" << currLabel["name"].string_value() << "]";
 				}
 				logfile << endl;
+				
+				if( currItem["comments"].int_value() > 0 )
+				{
+					stringstream	issuecommentfoldername;
+					issuecommentfoldername << "issues/" << bugNumber << "_comments";
+					mkdir( issuecommentfoldername.str().c_str(), 0777);
+					
+					string			commentsdata;
+					ifstream		downloadedcommentsjson;
+					stringstream	downloadedcommentsfilename;
+					downloadedcommentsfilename << bugNumber << "_comments_downloaded.json";
+					downloadedcommentsjson.open(downloadedcommentsfilename.str());
+					if( downloadedcommentsjson.is_open() )
+					{
+						cout << "Using cached data from "<< downloadedcommentsfilename.str() << endl;
+						commentsdata = file_contents( downloadedcommentsjson );
+					}
+					else
+					{
+						url_request	request;
+						url_reply	reply;
+						
+						request.add_header( "User-Agent: bugmatic/0.1" );
+					
+						CURLcode	errcode = request.load( currItem["comments_url"].string_value(), reply );
+						if( errcode == CURLE_OK )
+						{
 
+							logfile << "status: " << reply.status() << endl;
+							logfile << "type: " << reply.content_type() << endl;
+							vector<string> headers = reply.headers();
+
+							for(vector<string>::iterator it = headers.begin();
+									it != headers.end(); it++)
+								logfile << "Header: " << (*it) << endl;
+
+							ofstream	jsonfile( downloadedcommentsfilename.str() );
+							commentsdata = reply.data();
+							jsonfile << commentsdata;
+						}
+						else
+							cerr << "Curl Error: " << errcode << endl;
+					}
+					
+					errMsg.erase();
+					Json	commentsJson = Json::parse( commentsdata, errMsg );
+					if( errMsg.length() == 0 )
+					{
+						for( const Json& currComment : commentsJson.array_items() )
+						{
+							stringstream	issuecommentfilename;
+							issuecommentfilename << issuecommentfoldername.str() << "/" << currComment["id"].int_value() << ".json";
+							string			fn = issuecommentfilename.str();
+							ofstream	commentfile(fn);
+							string		fc = currComment.dump();
+							commentfile << fc;
+						}
+					}
+					else
+					{
+						cerr << "Couldn't write comments for bug #" << bugNumber << endl;
+						logfile << "Couldn't write comments for bug #" << bugNumber << endl;
+					}
+				}
+				stringstream	issuefilename;
+				issuefilename << "issues/" << bugNumber << ".json";
+				ofstream		issuefile( issuefilename.str() );
+				issuefile << currItem.dump();
+				
 				logfile << "----------" << endl;
 			}
 		}
