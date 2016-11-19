5ea664ffad25ebcfa41934da6b101c196732126b
Uli Kusterer <ulivc@zathras.de>
2016-11-19 14:55:13 +0100

    We now run the bugmatic_tests app when building the bugmatic tool, but the tests don't yet verify they've passed.

--- bugmatic_tests/main.cpp
+++ bugmatic_tests/main.cpp
@@ -7,10 +7,27 @@
 //
 
 #include <iostream>
+#include <vector>
+#include "bugmatic.hpp"
+#include "fake_filesystem.hpp"
+
+
+using namespace std;
+using namespace bugmatic;
+using namespace fake;
+
 
 int main(int argc, const char * argv[])
 {
-	// insert code here...
-	std::cout << "Hello, World!\n";
+	filesystem::path	test1path( filesystem::path(argv[0]).parent_path() / "data" / "testdata1" );
+	working_copy		wc( test1path.string() );
+	
+	cout << "note: ===== Find all uliwitness' bugs =====" << endl;
+	wc.list( (std::vector<std::string>){ "assignee.login=uliwitness" } );
+
+	cout << "note: ===== Find unclosed bugs in range 1...3 =====" << endl;
+	wc.list( (std::vector<std::string>){ "closed_at=null", "number<4" } );
+	
+	cout << "note: ===== Done. =====" << endl;
     return 0;
 }
